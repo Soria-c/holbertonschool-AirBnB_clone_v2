6f8b0d83d982ffa661f081c82ee5743181198b43
LesLor <leslie.paz.am@gmail.com>
2022-08-08 23:18:54 +0000

    Implementing DB Storage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from models.base_model import BaseModel, Base 
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+    #name = ""
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Getter attribute cities that 
+            returns the list of City instances
+            with state_id equals to the current State.id """
+            return list(filter(
+                    lambda x: x.state_id == self.id ,storage.all(
+                    City).values()))
