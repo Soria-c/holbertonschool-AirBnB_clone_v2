b4cec63343e918f5e0749428bcbafcdf9859ce66
LesLor <leslie.paz.am@gmail.com>
2022-08-09 02:51:27 +0000

    Updating city, user, review and place classes for DB

--- models/city.py
+++ models/city.py
@@ -2,11 +2,13 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base 
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """class definition of a City"""
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete")
     #state_id = ""
     #name = ""
