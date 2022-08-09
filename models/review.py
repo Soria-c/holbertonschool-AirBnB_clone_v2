56c462e3c600ae0bababfeac88e8e99b76e9f76f
Carlos Soria <soriac367@gmail.com>
2022-08-09 10:44:05 -0500

    Debug

--- models/review.py
+++ models/review.py
@@ -6,10 +6,11 @@ from sqlalchemy.orm import relationship
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = "review"
+    __tablename__ = "reviews"
+    text = Column(String(128), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    text = Column(String(128), nullable=False)
+    
 
     #place_id = ""
     #user_id = ""
