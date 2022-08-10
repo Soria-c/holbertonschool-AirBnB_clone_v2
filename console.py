ac6d856dc2fa84f3b88d792bec0b351b52426737
Carlos Soria <soriac367@gmail.com>
2022-08-10 00:23:11 -0500

    .

--- console.py
+++ console.py
@@ -248,7 +248,7 @@ class HBNBCommand(cmd.Cmd):
                 if k.split('.')[0] == args:
                     print_list.append(v)
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(v)
 
         print(print_list)
