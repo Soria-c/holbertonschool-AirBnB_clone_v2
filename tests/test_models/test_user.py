3b9af57728f23d4c059651dd0eb215e22e98f3a4
Carlos Soria <soriac367@gmail.com>
2022-08-10 14:23:01 -0500

    Updated tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,10 +3,20 @@
 from models.user import User
 import unittest
 import os
+import MySQLdb
+
+conn_params = {
+    'user': os.getenv("HBNB_MYSQL_USER"),
+    'passwd': os.getenv("HBNB_MYSQL_PWD"),
+    'host': os.getenv("HBNB_MYSQL_HOST"),
+    'db': os.getenv("HBNB_MYSQL_DB"),
+    'port': 3306
+}
 
 class test_User(unittest.TestCase):
     """ """
-
+    
+    cursor = None
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
@@ -32,5 +42,20 @@ class test_User(unittest.TestCase):
         """ """
         new = self.value()
         self.assertEqual(type(new.password), str)
+        
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db', "not testing db storage")
+    def setUp(self):
+        conn = MySQLdb.connect(**conn_params)
+        test_User.cursor = conn.cursor()
+        query = "CREATE DATABASE IF NOT EXISTS hbnb_test_db"
+        test_User.cursor.execute(query)
+
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db', "not testing fl storage")
+    def test_sql(self):
+        
+        query = "SELECT COUNT(*) FROM users"
+        test_User.cursor.execute(query)
+        self.assertEqual(0, list(test_User.cursor)[0][0])
     
 
