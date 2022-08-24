cc124f1a92b3a0d5cdc55674fcfe5028ae57aca0
Carlos Soria <soriac367@gmail.com>
2022-08-24 18:26:34 -0500

    Prepares servers for deployment

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Setting up for deployment
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install nginx -y
+
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared
+sudo chown -R ubuntu:ubuntu /data
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+
+echo "test" >> /data/web_static/releases/test/index.html
+
+sudo sed -i "/server_name _;/a \\\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}\n" /etc/nginx/sites-available/default
+
+sudo service nginx restart
