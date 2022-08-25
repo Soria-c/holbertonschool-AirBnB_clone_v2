a14f355ae5db6ebbb0ef1e090b0796b91e25115c
Carlos Soria Otárola <98144875+Soria-c@users.noreply.github.com>
2022-08-25 16:25:25 -0500

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,17 +11,20 @@ def do_deploy(archive_path):
     """Script to send files to deploy servers an arrange them"""
     if not(exists(archive_path)):
         return False
-    file_name = archive_path.split('/')[-1]
-    new_name = file_name.strip('.tgz')
-    dir_name = "/data/web_static/releases/" + new_name
+    try:
+        file_name = archive_path.split('/')[-1]
+        new_name = file_name.strip('.tgz')
+        dir_name = "/data/web_static/releases/" + new_name
 
-    put(archive_path, '/tmp')
-    run('mkdir -p {}'.format(dir_name))
-    run('tar -zvxf /tmp/{} -C {}'.format(file_name, dir_name))
-    run('sudo rm /tmp/{}'.format(file_name))
-    run('rm /data/web_static/current')
-    run('ln -sf {} /data/web_static/current'.format(dir_name))
-    run('mv {}/web_static/* {}'.format(dir_name, dir_name))
-    run('rm -r {}/web_static/'.format(dir_name))
-    print('New version deployed!')
-    return True
+        put(archive_path, '/tmp')
+        run('mkdir -p {}'.format(dir_name))
+        run('tar -zvxf /tmp/{} -C {}'.format(file_name, dir_name))
+        run('sudo rm /tmp/{}'.format(file_name))
+        run('rm /data/web_static/current')
+        run('ln -sf {} /data/web_static/current'.format(dir_name))
+        run('mv {}/web_static/* {}'.format(dir_name, dir_name))
+        run('rm -r {}/web_static/'.format(dir_name))
+        print('New version deployed!')
+        return True
+     except Exception:
+        return False
