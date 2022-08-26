201843780d668e73bbb212c501625af1199249f6
Carlos Soria <soriac367@gmail.com>
2022-08-25 20:40:24 -0500

    ?

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,8 +8,9 @@ sudo apt install nginx -y
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared
 
 echo "test" | sudo tee /data/web_static/releases/test/index.html
-sudo chown -R ubuntu:ubuntu /data
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+
 
 sudo sed -i "/server_name _;/a \\\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}\n" /etc/nginx/sites-available/default
 
